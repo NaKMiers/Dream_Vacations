90a946ee8deed5c9305a66a59ee762afa8a92888
Pi Pi <diwas118151@gmail.com>
2023-09-24 16:30:47 +0700

    completed Advertising => completed ContactPage

--- src/components/Advertising/index.jsx
+++ src/components/Advertising/index.jsx
@@ -1,16 +1,29 @@
 import React, { memo } from 'react'
 import styles from './styles.module.scss'
 import SeparatorTitle from '../../components/SeparatorTitle'
+import advertiseBanner from '../../assets/images/advertiseBanner2.jpg'
 
 function Advertising() {
    return (
       <section className={styles.Advertising}>
          <div className={`${styles.container} container`}>
-            <SeparatorTitle
-               title='Advertise With Us'
-               dark
-               style={{ marginBottom: '1.1rem', background: 'aquamarine' }}
-            />
+            <SeparatorTitle title='Advertise With Us' dark style={{ marginBottom: '30px' }} />
+
+            <p className={styles.paragraph}>
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               exercitationtempor incididunt ut labore et dolore magna aliqua enim!
+            </p>
+            <p className={styles.paragraph}>
+               If you would like to advertise with us, please contact our Digital Sales Planner:
+            </p>
+
+            <h3 className={styles.email}>
+               <a href='mailto:advertising@domain.ltd'>advertising@domain.ltd</a>
+            </h3>
+
+            <div className={styles.advertiseBanner}>
+               <img src={advertiseBanner} alt='advertise-banner' />
+            </div>
          </div>
       </section>
    )
--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -52,7 +52,6 @@ function ContactPage() {
          />
 
          <GetInTouch />
-
          <Advertising />
       </div>
    )
